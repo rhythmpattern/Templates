7b8a86b0b1788b2de35041a853337ae56af60c0e
rhythmpattern <hoseshaun@gmail.com>
2023-04-09 13:12:58 +1000

    refactored resize, render an dupate

---
+++ assets/tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileset2" tilewidth="20" tileheight="20" spacing="2" margin="2" tilecount="961" columns="31">
+ <image source="tileset.png" width="692" height="692"/>
+</tileset>
