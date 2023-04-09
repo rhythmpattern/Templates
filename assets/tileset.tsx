dec60241884f186f3c1bb8b3198f334ddcfcfc90
rhythmpattern <hoseshaun@gmail.com>
2023-04-09 10:22:00 +1000

    added some tiled files

---
+++ assets/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileset" tilewidth="21" tileheight="21" spacing="2" margin="1" tilecount="900" columns="30">
+ <image source="../../DevCrossSource/libgdx-cookbook-master/samples/android/assets/data/maps/tileset.png" width="692" height="692"/>
+</tileset>
