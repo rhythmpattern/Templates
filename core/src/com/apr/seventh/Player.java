cfa64040405c77c6d334f6e352e161892b83bf23
rhythmpattern <hoseshaun@gmail.com>
2023-04-09 20:24:29 +1000

    removed mesh

--- core/src/com/apr/seventh/Player.java
+++ core/src/com/apr/seventh/Player.java
@@ -43,40 +43,4 @@ public class Player {
         sprite.draw(spriteBatch);
     }
 
-    public Mesh createFullScreenQuad() {
-
-        float[] verts = new float[20];
-        int i = 0;
-
-        verts[i++] = -1; // x1
-        verts[i++] = -1; // y1
-        verts[i++] = 0;
-        verts[i++] = 0f; // u1
-        verts[i++] = 0f; // v1
-
-        verts[i++] = 1f; // x2
-        verts[i++] = -1; // y2
-        verts[i++] = 0;
-        verts[i++] = 1f; // u2
-        verts[i++] = 0f; // v2
-
-        verts[i++] = 1f; // x3
-        verts[i++] = 1f; // y3
-        verts[i++] = 0;
-        verts[i++] = 1f; // u3
-        verts[i++] = 1f; // v3
-
-        verts[i++] = -1; // x4
-        verts[i++] = 1f; // y4
-        verts[i++] = 0;
-        verts[i++] = 0f; // u4
-        verts[i++] = 1f; // v4
-
-        Mesh mesh = new Mesh( true, 4, 0,  // static mesh with 4 vertices and no indices
-                new VertexAttribute( VertexAttributes.Usage.Position, 3, ShaderProgram.POSITION_ATTRIBUTE ),
-                new VertexAttribute( VertexAttributes.Usage.TextureCoordinates, 2, ShaderProgram.TEXCOORD_ATTRIBUTE+"0" ) );
-
-        mesh.setVertices( verts );
-        return mesh;
-    }
 }
